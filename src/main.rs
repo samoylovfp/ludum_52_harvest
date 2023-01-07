6e185c93c40bbcc1277a13e15a7f4ee647eb77b9
Filipp Samoilov <samoylovfp@gmail.com>
2023-01-07 10:09:23 +0200

    Bouncing rect from bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+//! Exampel from Bevy 2D
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(sprite_movement)
+        .run();
+}
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::Rgba {
+                    red: 1.0,
+                    green: 0.0,
+                    blue: 0.0,
+                    alpha: 1.0,
+                },
+                custom_size: Some(Vec2 { x: 300.0, y: 300.0 }),
+                ..default()
+            },
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        Direction::Up,
+    ));
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in &mut sprite_position {
+        match *logo {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
+
+        if transform.translation.y > 200. {
+            *logo = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *logo = Direction::Up;
+        }
+    }
 }
