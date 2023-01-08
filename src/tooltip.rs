4fa652b210e39d68183a3082079a30f296733ae1
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-01-08 09:35:47 +0200

    Remove dbg from tooltip

--- src/tooltip.rs
+++ src/tooltip.rs
@@ -61,7 +61,6 @@ pub fn update_tooltip(
         tooltip.translation.x = world_pos.x + 10.0;
         tooltip.translation.y = world_pos.y - 10.0;
         tooltip.translation.z = 2.0;
-        dbg!(tooltip.translation);
     }
 
     text.sections[0].value = "".to_string();
