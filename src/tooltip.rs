8c3a75f3dde7c70cab3499834cee45e0e978f88b
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-01-08 10:11:49 +0200

    Fix tooltip bug

--- src/tooltip.rs
+++ src/tooltip.rs
@@ -45,39 +45,34 @@ pub fn update_tooltip(
         wnds.get_primary().unwrap()
     };
 
-    let mut world_pos = Vec3 {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-    };
+	text.sections[0].value = "".to_string();
 
     if let Some(screen_pos) = wnd.cursor_position() {
         let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
         let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
         let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
-        world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+        let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
         // let world_pos: Vec2 = world_pos.truncate();
 
         tooltip.translation.x = world_pos.x + 10.0;
         tooltip.translation.y = world_pos.y - 10.0;
         tooltip.translation.z = 2.0;
-    }
 
-    text.sections[0].value = "".to_string();
-
-    for (object, sprite, string) in objects.iter() {
-        if collide(
-            object.translation,
-            Vec2 {
-                x: sprite.custom_size.unwrap().x,
-                y: sprite.custom_size.unwrap().y,
-            },
-            world_pos,
-            Vec2 { x: 1.0, y: 1.0 },
-        )
-        .is_some()
-        {
-            text.sections[0].value = string.0.clone();
-        }
+		for (object, sprite, string) in objects.iter() {
+			if collide(
+				object.translation,
+				Vec2 {
+					x: sprite.custom_size.unwrap().x,
+					y: sprite.custom_size.unwrap().y,
+				},
+				world_pos,
+				Vec2 { x: 1.0, y: 1.0 },
+			)
+			.is_some()
+			{
+				text.sections[0].value = string.0.clone();
+			}
+		}
     }
+    
 }
